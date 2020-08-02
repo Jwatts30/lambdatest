cb8f6ac3ab6fe3d726d36b1aebfe9e78bd62b1b2
Jwatts30 <jannatwatts999@gmail.com>
2020-08-02 21:46:59 +0530

    gitignore file

--- docker-boto3/shell.sh
+++ docker-boto3/shell.sh
@@ -1,7 +1,9 @@
 #/usr/bin/env bash
+cd sel-firefox
+docker build -t firefox-selenium .
 cd sel-chrome
 docker build -t chrome-selenium .
 cd ../sel-firefox
 docker build -t firefox-selenium .
-docker run -e url="www.facebook.com" chrome-selenium
+docker run -e url=$1 chrome-selenium
 docker run -e url="www.facebook.com" firefox-selenium
